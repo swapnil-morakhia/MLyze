64b5d9a5bc2ee44db7d22daf7458522f0ef24f55
Saksham Tiwari <contact@tsaksham.com>
2021-05-06 17:27:05 -0400

    Final commit (hopefully)

--- GoogleImageScrapper.py
+++ GoogleImageScrapper.py
@@ -8,11 +8,9 @@ class GoogleImageScraper:
     def __init__(self, webdriver_path, image_path, search_key="cat", number_of_images=1, headless=False, min_resolution=(0, 0), max_resolution=(1920, 1080)):
         # Check parameter types.
         if type(number_of_images) != int:
-            print("GoogleImageScraper Error: Number of images must be integer value.")
             return
 
         if not os.path.exists(image_path):
-            print("GoogleImageScraper Notification: Image path not found. Creating a new folder.")
             os.makedirs(image_path)
 
         self.search_key = search_key
@@ -27,7 +25,6 @@ class GoogleImageScraper:
         self.valid_extensions = ["jpg", "png", "jpeg"]
 
     def find_image_urls(self):
-        print("[+] Scraping for image link... Please wait.")
         image_urls = []
         count = 0
         missed_count = 0
@@ -42,7 +39,7 @@ class GoogleImageScraper:
             driver.minimize_window
             driver.get(self.url)
         except Exception:
-            print("[-] Please update the chromedriver.exe in the webdriver folder according to your chrome version:https://chromedriver.chromium.org/downloads")
+            pass
 
         for indx in range(1, self.number_of_images + 1):
             try:
@@ -51,10 +48,8 @@ class GoogleImageScraper:
                 imgurl.click()
                 missed_count = 0
             except Exception:
-                # print("[-] Unable to click this photo.")
                 missed_count = missed_count + 1
                 if missed_count > 10:
-                    print("[+] No more photos.")
                     break
                 else:
                     continue
@@ -67,33 +62,28 @@ class GoogleImageScraper:
                 for image in images:
                     # only downloads images that starts with http
                     if image.get_attribute("src")[:4].lower() in ["http"]:
-                        print("%d. %s" % (count, image.get_attribute("src")))
                         image_urls.append(image.get_attribute("src"))
                         count += 1
                         break
             except Exception:
-                print("[-] Unable to get link")
+                pass
 
             try:
                 # scroll page to load next image
                 driver.execute_script("window.scrollTo(0, " + str(indx * 100) + ");")
                 element = driver.find_element_by_class_name("mye4qd")
                 element.click()
-                print("[+] Loading more photos")
             except Exception:
                 pass
 
         driver.close()
-        print("[+] Google search ended")
         return image_urls
 
     def save_images(self, image_urls):
-        print("[+] Saving Image... Please wait.")
         for indx, image_url in enumerate(image_urls):
             try:
                 filename = "%s%s.%s" % (self.search_key, str(indx), self.saved_extension)
                 image_path = os.path.join(self.image_path, filename)
-                print("%d .Image saved at: %s" % (indx, image_path))
                 image = requests.get(image_url)
                 if image.status_code == 200:
                     with open(image_path, 'wb') as f:
@@ -107,10 +97,7 @@ class GoogleImageScraper:
                                     self.min_resolution[1] or image_resolution[0] > self.max_resolution[0] or \
                                     image_resolution[1] > self.max_resolution[1]:
                                 image_from_web.close()
-                                # print("GoogleImageScraper Notification: %s did not meet resolution requirements."%(image_url))
                                 os.remove(image_path)
                         image_from_web.close()
             except Exception as e:
-                print("GoogleImageScraper Error: Failed to be downloaded.", e)
                 pass
-        print("[+] Download Completed. Please note that some photos is not downloaded as it is not in the right format (e.g. jpg, jpeg, png)")
